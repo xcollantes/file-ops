3e9fb9dc89d76d5e26eb24ca7c6d538919855516
xcollantes <xcollantes@users.noreply.github.com>
2017-08-08 01:29:42 -0700

    Add function
    Created getPaths() function

--- fileOps.py
+++ fileOps.py
@@ -1,17 +1,16 @@
 # Created by Xavier Collantes
 # 6/22/2017
 # File: fileOps.py
-# Purpose: Script to copy all leaf files child of this relative script location
-# to single specified path directory.  
 
 
 import os, shutil, sys
-
 
 def copy_file(pathFrom, pathTo):
     shutil.copy(pathFrom, pathTo)
     print (pathFrom + " file copied to " + pathTo)
 
+# Purpose: Script to copy all leaf files child of this relative script location
+# to single specified path directory.  
 def traverse(full_path_root):
     os.chdir(full_path_root) #Change dir to root
     
@@ -24,7 +23,22 @@ def traverse(full_path_root):
 
             
             print (os.path.join(full_path_root, item) + " - processed")
-       
+
+# pre: Directory with contents
+# post: output file populated with directory file names
+# purpose: output all filenames to a specified file 
+def getPaths(full_path, destination):
+    os.chdir(full_path)
+    
+    #File Operations 
+    fo = open(destination, 'w+')
+
+    for i in os.listdir(full_path):
+        raw = str(i)
+        fo.write(raw + ", " + "\n")
+
+    fo.close()
+    
 def main():
     #ENTER DIRECTORY PATH ORIGIN/DESTINATION HERE (WHERE YOU WANT THE FILES)
     #origin is optional, default is position of script file
@@ -32,7 +46,8 @@ def main():
     destination = ''
 
 
-
+    print ("Origin: " + specified_origin)
+    print ("Destination: " + destination)
     start = input("Confirm origin/destination paths (y/n)?")
     if (start == 'Y' or start == 'y'):
         if (specified_origin == ''):
@@ -41,7 +56,11 @@ def main():
             start_path = specified_origin
     
         print ("Starting in " + start_path)
-        traverse(start_path)
+        #traverse(start_path)
+        getPaths(specified_origin, destination)
+
+
+        
     else:
         print ("Please check paths are correct in src file.")
         
